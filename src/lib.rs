228fd4adfd25c69d2e4a42b3e11691300f86dfbe
Chris McComb <ccmcc2012@gmail.com>
2021-10-30 19:14:02 -0400

    Small update to documentation

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,12 @@
 #![warn(rustdoc::missing_doc_code_examples)]
 #![warn(clippy::missing_docs_in_private_items)]
 
-//! # Regex for Humans
-//! ## About
+//! # A Regex for Humans
 //! The goal of this crate is simple: give everybody the power of regular expressions without having
 //! to learn the complicated syntax. It is inspired by [ReadableRegex.jl](https://github.com/jkrumbiegel/ReadableRegex.jl).
 
-//!## Example usage
-//!### Matching a date
+//!# Example usage
+//!## Matching a date
 //!If you want to match a date of the format `2021-10-30`, you would use the following code to generate a regex:
 //!```rust
 //!let hr = human_regex::HumanRegex::new()
