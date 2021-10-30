315bb04df2c3e7c1b3147360a4b9a38c8e447b14
Chris McComb <ccmcc2012@gmail.com>
2021-10-30 17:57:26 -0400

    We have functionality! Woo!

--- tests/test.rs
+++ tests/test.rs
@@ -6,18 +6,21 @@ mod tests {
         let check_match = human_regex::HumanRegex::from_regex_string(r"^\d{4}-\d{2}-\d{2}$");
         assert!(check_match.is_match("2014-01-01"))
     }
+
     #[test]
     fn check_default() {
         let check_match = human_regex::HumanRegex::default();
         assert!(check_match.is_match(""))
     }
+
     #[test]
     fn check_new() {
         let check_match = human_regex::HumanRegex::new();
         assert!(check_match.is_match(""))
     }
+
     #[test]
-    fn check_exactly_plus_text() {
+    fn match_date() {
         let check_match = human_regex::HumanRegex::new()
             .begin()
             .exactly(4, human_regex::DIGIT)
@@ -28,4 +31,18 @@ mod tests {
             .end();
         assert!(check_match.is_match("2014-01-01"))
     }
+}
+
+#[test]
+#[should_panic]
+fn match_date_should_panic() {
+    let check_match = human_regex::HumanRegex::new()
+        .begin()
+        .exactly(4, human_regex::DIGIT)
+        .text("-")
+        .exactly(2, human_regex::DIGIT)
+        .text("-")
+        .exactly(2, human_regex::DIGIT)
+        .end();
+    assert!(check_match.is_match("01-01-2014"))
 }
