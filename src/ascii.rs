32a04d9ddf39d863a34568c7b845c92d4b587b00
Chris McComb <ccmcc2012@gmail.com>
2023-02-12 11:10:52 -0500

    Add some spaces to make rustfmt happy

--- src/ascii.rs
+++ src/ascii.rs
@@ -6,6 +6,7 @@ use super::humanregex::HumanRegex;
 pub fn alphanumeric() -> HumanRegex {
     HumanRegex(r"[[:alnum:]]".to_string())
 }
+
 /// A function to match any non-alphanumeric character (`[^0-9A-Za-z]`)
 pub fn non_alphanumeric() -> HumanRegex {
     HumanRegex(r"[[:^alnum:]]".to_string())
@@ -15,6 +16,7 @@ pub fn non_alphanumeric() -> HumanRegex {
 pub fn alphabetic() -> HumanRegex {
     HumanRegex(r"[[:alpha:]]".to_string())
 }
+
 /// A function to match any non-alphabetic character (`[^A-Za-z]`)
 pub fn non_alphabetic() -> HumanRegex {
     HumanRegex(r"[[:^alpha:]]".to_string())
@@ -24,6 +26,7 @@ pub fn non_alphabetic() -> HumanRegex {
 pub fn lowercase() -> HumanRegex {
     HumanRegex(r"[[:lower:]]".to_string())
 }
+
 /// A function to match any non-lowercase character (`[^a-z]`)
 pub fn non_lowercase() -> HumanRegex {
     HumanRegex(r"[[:^lower:]]".to_string())
@@ -33,6 +36,7 @@ pub fn non_lowercase() -> HumanRegex {
 pub fn uppercase() -> HumanRegex {
     HumanRegex(r"[[:upper:]]".to_string())
 }
+
 /// A function to match any non-uppercase character (`[^A-Z]`)
 pub fn non_uppercase() -> HumanRegex {
     HumanRegex(r"[[:^upper:]]".to_string())
@@ -42,6 +46,7 @@ pub fn non_uppercase() -> HumanRegex {
 pub fn hexdigit() -> HumanRegex {
     HumanRegex(r"[[:xdigit:]]".to_string())
 }
+
 /// A function to match any digit that wouldn't appear in a hexadecimal number (`[^A-Fa-f0-9]`)
 pub fn non_hexdigit() -> HumanRegex {
     HumanRegex(r"[[:^xdigit:]]".to_string())
@@ -51,6 +56,7 @@ pub fn non_hexdigit() -> HumanRegex {
 pub fn ascii() -> HumanRegex {
     HumanRegex(r"[[:ascii:]]".to_string())
 }
+
 /// A function to match any non-ascii digit (`[^\x00-\x7F]`)
 pub fn non_ascii() -> HumanRegex {
     HumanRegex(r"[[:^ascii:]]".to_string())
@@ -60,6 +66,7 @@ pub fn non_ascii() -> HumanRegex {
 pub fn blank() -> HumanRegex {
     HumanRegex(r"[[:blank:]]".to_string())
 }
+
 /// A function to match non-blank characters (`[^\t ]`)
 pub fn non_blank() -> HumanRegex {
     HumanRegex(r"[[:^blank:]]".to_string())
@@ -69,6 +76,7 @@ pub fn non_blank() -> HumanRegex {
 pub fn control() -> HumanRegex {
     HumanRegex(r"[[:cntrl:]]".to_string())
 }
+
 /// A function to match non-control characters (`[^\x00-\x1F\x7F]`)
 pub fn non_control() -> HumanRegex {
     HumanRegex(r"[[:^cntrl:]]".to_string())
@@ -78,6 +86,7 @@ pub fn non_control() -> HumanRegex {
 pub fn graphical() -> HumanRegex {
     HumanRegex(r"[[:graph:]]".to_string())
 }
+
 /// A function to match non-graphical characters (`[^!-~]`)
 pub fn non_graphical() -> HumanRegex {
     HumanRegex(r"[[:^graph:]]".to_string())
@@ -87,6 +96,7 @@ pub fn non_graphical() -> HumanRegex {
 pub fn printable() -> HumanRegex {
     HumanRegex(r"[[:print:]]".to_string())
 }
+
 /// A function to match unprintable characters (`[^ -~]`)
 pub fn non_printable() -> HumanRegex {
     HumanRegex(r"[[:^print:]]".to_string())
@@ -96,6 +106,7 @@ pub fn non_printable() -> HumanRegex {
 pub fn punctuation() -> HumanRegex {
     HumanRegex(r"[[:punct:]]".to_string())
 }
+
 /// A function to match non-punctuation (`[^!-/:-@\[-`{-~]`)
 pub fn non_punctuation() -> HumanRegex {
     HumanRegex(r"[[:^punct:]]".to_string())
