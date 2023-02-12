751bc0750e8f33c31b7cdf007da91ff0bd63410b
Chris McComb <ccmcc2012@gmail.com>
2023-02-12 11:18:12 -0500

    Two example date matches

--- examples/match_date.rs
+++ examples/match_date.rs
@@ -1,12 +1,22 @@
 use human_regex::{beginning, digit, end, exactly, text};
 
 fn main() {
-    let regex_string = beginning()
+    // Build the first match pattern
+    let regex_string_1 = beginning()
         + exactly(4, digit())
         + text("-")
         + exactly(2, digit())
         + text("-")
         + exactly(2, digit())
         + end();
-    println!("{}", regex_string.to_regex().is_match("2014-01-01"));
+
+    // Build the second match pattern
+    let regex_string_2 =
+        beginning() + exactly(4, digit()) + exactly(2, text("-") + exactly(2, digit())) + end();
+
+    // Check the match
+    println!("{}", regex_string_1.to_regex().is_match("2014-01-01"));
+
+    // Check the match
+    println!("{}", regex_string_2.to_regex().is_match("2014-01-01"));
 }
