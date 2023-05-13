6aaafbad6e53d30afcf7955756abd3c23a85829f
Chris McComb <ccmcc2012@gmail.com>
2023-05-13 11:22:13 -0400

    Cleaning up uppercase tests

--- src/ascii.rs
+++ src/ascii.rs
@@ -113,6 +113,10 @@ pub fn non_lowercase() -> HumanRegex<SymbolClass<Ascii>> {
 /// assert!(regex_string.to_regex().is_match("a").not());
 /// assert!(regex_string.to_regex().is_match("A"));
 /// assert!(regex_string.to_regex().is_match("1").not());
+/// assert!(regex_string.to_regex().is_match("¡").not());
+/// assert!(regex_string.to_regex().is_match("!").not());
+/// assert!(regex_string.to_regex().is_match(" ").not());
+/// assert!(regex_string.to_regex().is_match("\n").not());
 /// ```
 pub fn uppercase() -> HumanRegex<SymbolClass<Ascii>> {
     HumanRegex(r"[[:upper:]]".to_string(), pd::<SymbolClass<Ascii>>)
@@ -126,6 +130,10 @@ pub fn uppercase() -> HumanRegex<SymbolClass<Ascii>> {
 /// assert!(regex_string.to_regex().is_match("a"));
 /// assert!(regex_string.to_regex().is_match("A").not());
 /// assert!(regex_string.to_regex().is_match("1"));
+/// assert!(regex_string.to_regex().is_match("¡"));
+/// assert!(regex_string.to_regex().is_match("!"));
+/// assert!(regex_string.to_regex().is_match(" "));
+/// assert!(regex_string.to_regex().is_match("\n"));
 /// ```
 pub fn non_uppercase() -> HumanRegex<SymbolClass<Ascii>> {
     HumanRegex(r"[[:^upper:]]".to_string(), pd::<SymbolClass<Ascii>>)
